9b8bce66ebbbb47bf75f8fc2fb791c8bfb69ee29
rossalai <rossalai@msu.edu>
2017-01-12 16:53:14 -0500

    made source dir

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: alaina
+ *
+ * Created on January 12, 2017, 4:16 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
