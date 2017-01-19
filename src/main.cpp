51ce0e11b22238e0cc9ad577d31fcabef0bedd7d
rossalai <rossalai@msu.edu>
2017-01-19 16:48:30 -0500

    finished back substitution

--- src/main.cpp
+++ src/main.cpp
@@ -26,29 +26,52 @@ int main(int argc, char** argv) {
     
     //initialize matrix and vector
     for (int i=0;i<n;i++){
-        b(i)=i-1;
+        b(i)=i;
         for (int j=0;j<n;j++){
-            a(i,j)=i+j+1;
-            if(i==1 && j ==1)
+            a(i,j)=i+j+2;
+            if(i==0 && j ==0)
                 a(i,j)=1;
         }
     }
     
+    cout<<"Before Gauss elimination"<<endl;
+    print_vals(a,b,x,n);
+    
+    //do forward elimination
     double temp=0;
     for (int k=0;k<=n-2;k++){
-        cout<<"1"<<endl;
         for (int i=k+1;i<=n-1;i++){
-            cout<<"2"<<endl;
             temp=a(i,k)/a(k,k);
             for ( int j=k;j<=n-1;j++){
-                cout<<"3"<<endl;
                 a(i,j)=a(i,j)-a(k,j)*temp;
             }
             b(i)=b(i)-temp*b(k);
+            if(abs(b(i))<=pow(10,-14))
+                b(i)=0;
         }
     }
+
+    //do back substitution
+    if(abs(a(n-1,n-1))<=pow(10,-14)){
+        x(n-1)=1;
+        a(n-1,n-1)=0;
+    }
+    else{
+        x(n-1)=b(n-1)/a(n-1,n-1);
+    }
+    double sum=0;
+    for (int i=n-2;i>=0;i--){
+        for (int j=n-1;j>i;j--){
+            sum+=a(i,j)*x(j);
+        }
+        x(i)=(b(i)-sum)/a(i,i);
+        if(abs(x(i))<=pow(10,-14))
+            x(i)=0;
+        sum=0;
+    }
     
-    print_vals(a,b,x,n);
+    cout<<"After Gauss elimination"<<endl;
+    print_vals(a,b,x,n); 
     
     
     return 0;
